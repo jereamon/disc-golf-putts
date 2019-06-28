5345c15661bb0e9e02535292e701dc896c41fd9f
Jeremy <jereamon@gmail.com>
2019-06-27 21:43:54 -0700

    Messed up in main.py. Fixed can't call environ, changed to environ.get.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from model import PuttSesh, Putt
 
 app = Flask(__name__)
 # app.secret_key = b'\x9d\xb1u\x08%(hAh\xa4\xcdw\x12S*,u\xec\xb8\xb8'
-app.secret_key = os.environ('SECRET_KEY').encode()
+app.secret_key = os.environ.get('SECRET_KEY').encode()
 
 distances = [18, 21, 24, 27, 30, 33]
 
