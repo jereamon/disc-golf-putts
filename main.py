38af7e9458df745057ad9da136fa4b4110c49900
Jeremy <jereamon@gmail.com>
2019-06-29 23:23:32 -0700

    All sorts of styling changes. Everything looks a little better now.

--- main.py
+++ main.py
@@ -146,5 +146,5 @@ def view_puttsesh_single(sesh_id):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6789))
-    # app.run(host='0.0.0.0', port=port, debug=True)
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
+    # app.run(host='0.0.0.0', port=port)
